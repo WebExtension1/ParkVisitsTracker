78f383ad3df7e859d9ac86a93f2b3c76b5e80173
Web Extension <robertjenner5@outlook.com>
2024-10-23 18:58:12 +0100

    things

--- portfolio-file.php
+++ portfolio-file.php
@@ -17,20 +17,15 @@
     <a href="https://github.com/WebExtension1" style="position: absolute; right: 75px; top: 75px; color: white; border: solid 2px white; padding: 10px; text-decoration: none;" target="_blank">GitHub</a>
     <div style="display: flex; flex-direction: row; gap: 10vw">
         <div>
-            <h1 style="margin: calc(50vh - 90px) 0px 5px 45px; color: white; font-size: 50px;" class="slide1">Robert Jenner</h1>
-            <h3 style="margin: 5px 0px 0px 65px; color: white; font-size: 29px; font-weight: normal;" class="slide2">19 year-old Web Developer</h3>
-            <h5 style="margin: 7px 0px 0px 85px; color: white; font-size: 19px; font-weight: normal;" class="slide3">Studying Software Engineering at Sheffield Hallam University</h5>
+            <h1 style="margin: calc(50vh - 90px) 0px 5px 45px; color: white; font-size: 50px;" class="slide1 enlarge1">Robert Jenner</h1>
+            <h3 style="margin: 5px 0px 0px 65px; color: white; font-size: 29px; font-weight: normal;" class="slide2 enlarge1">19 year-old Software Engineer</h3>
+            <h5 style="margin: 7px 0px 0px 85px; color: white; font-size: 19px; font-weight: normal;" class="slide3 enlarge1">Studying Software Engineering at Sheffield Hallam University</h5>
         </div>
         <div>
-            <h1 style="margin: calc(50vh - 160px) 0px 5px 75px; font-size: 80px;" class="slide3"><a href="<?php echo $directoryString; ?>portfolio/projects" style="color:lightgray;">Projects</a></h1>
-            <h1 style="margin: 5px 0px 5px 135px; font-size: 80px;" class="slide4"><a href="<?php echo $directoryString; ?>portfolio/contact" style="color:lightgray;">Contact</a></h1>
-            <h1 style="margin: 5px 0px 5px 215px; font-size: 80px;" class="slide5"><a href="<?php echo $directoryString; ?>portfolio/about" style="color:lightgray;">About Me</a></h1>
+            <h1 style="margin: calc(50vh - 160px) 0px 5px 75px; font-size: 80px;" class="slide3 enlarge1"><a href="<?php echo $directoryString; ?>portfolio/projects" style="color:lightgray;">Projects</a></h1>
+            <h1 style="margin: 5px 0px 5px 135px; font-size: 80px;" class="slide4 enlarge1"><a href="<?php echo $directoryString; ?>portfolio/contact" style="color:lightgray;">Contact</a></h1>
+            <h1 style="margin: 5px 0px 5px 215px; font-size: 80px;" class="slide5 enlarge1"><a href="<?php echo $directoryString; ?>portfolio/about" style="color:lightgray;">About Me</a></h1>
         </div>
     </div>
-    <?php
-    $json = file_get_contents("data/portfolio.json");
-    $json_data = json_decode($json, true);
-    $projects = $json_data['projects'];
-    ?>
 </body>
 </html>
--- portfolio-projects-file.php
+++ portfolio-projects-file.php
@@ -9,9 +9,38 @@
     <style>body { font-family: 'Barlow Condensed', sans-serif; }</style>
     <link rel="stylesheet" href="<?php echo $directoryString; ?>css/mobile.css" />
     <link rel="stylesheet" href="<?php echo $directoryString; ?>css/desktop.css" media="only screen and (min-width : 790px)"/>
+    <link rel="stylesheet" href="<?php echo $directoryString; ?>css/portfolio.css" defer>
 </head>
 <body>
     <p><a href="<?php echo $directoryString; ?>portfolio" style="position: absolute; color: white; text-decoration: none; margin-left: 20px; margin-top: -20px;"><-- Home --</a></p>
     <h1 style="display:flex; justify-content: center; color: white; font-size: 60px;">Projects</h1>
+    <?php
+    $json = file_get_contents("data/portfolio.json");
+    $json_data = json_decode($json, true);
+    $projects = $json_data['projects'];
+    $keys = array_column($projects, 'priority');
+    array_multisort($keys, SORT_ASC, $projects);
+
+    function description($project) {
+        echo "<p style='color: white; font-size: 20px; margin-top: 50px;'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi praesentium quo! Ipsum distinctio consequatur aliquid exercitationem optio libero soluta quidem consectetur repellendus. Quis quam sit sed enim magni consequuntur harum eos nostrum molestias, cum suscipit officia quaerat. Porro vel maiores nostrum corrupti ab facere rerum nobis voluptate nihil libero?</p>";
+    }
+
+    function image($project, $directoryString) {
+        echo "<img class='enlarge2' style='color: white; border: solid 2px white; margin: 50px;' src='" . $directoryString . "images/" . $project['image'] . "' alt='" . $project['image'] . "'>";
+    }
+
+    foreach ($projects as $project) {
+        echo "<div class='project'><p style='color: white; font-size: 60px; margin: 10px 0px 5px 20px'>" . $project['name'] . "</p><div class='content' style='display: flex; flex-direction: row;'>";
+        if ($project['priority'] % 2 == 0) {
+            description($project);
+            image($project, $directoryString);
+        }
+        else {
+            image($project, $directoryString);
+            description($project);
+        }
+        echo "</div></div>";
+    }
+    ?>
 </body>
 </html>
